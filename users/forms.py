c52386482f2b9253d7aa1e7cfb6db7fb225b626c
davod <davoadeivazkhani@gmail.com>
2025-06-28 21:08:38 +0330

    fix: Correctly include phone_number in registration form fields

--- users/forms.py
+++ users/forms.py
@@ -7,4 +7,4 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = UserCreationForm.Meta.fields + ('email', 'first_name', 'last_name',)
+        fields = UserCreationForm.Meta.fields + ('phone_number', 'email', 'first_name', 'last_name',)
